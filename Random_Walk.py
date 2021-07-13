bef664351198cf4a0bc0373677bb1cee8426907a
Patrick O'Keefe <67643795+P-O-K@users.noreply.github.com>
2021-07-13 19:24:19 +0100

    Add files via upload

---
+++ Random_Walk.py
@@ -0,0 +1,42 @@
+
+from random import choice
+
+
+
+def simulateWalk( distance ) -> int:
+    ''' Returns "some" distance travelled from staring position '''
+    moveDirection = { 'N':[ 0, 1 ], 'S':[ 0, -1 ], 'E':[ 1, 0 ], 'W':[ -1, 0 ] }
+    currentPosition = [ 0, 0 ]
+
+    sumLists = lambda x,y: x+y
+    for _ in range( distance ):
+        currentPosition = list( map( sumLists, currentPosition, moveDirection[ choice( 'NSEW' ) ] ) )
+
+    return abs( currentPosition[ 0 ] ) +abs( currentPosition[ 1 ] )
+
+
+
+
+def findLongestWalk( withinRange, maxWalkLength, cyclesPerWalk ) -> int:
+	longestWalk = None
+
+	for DST in range( withinRange, maxWalkLength ):
+	    walksWithinRange = 0
+	    
+	    for _ in range( cyclesPerWalk ):
+	        if simulateWalk( DST ) <= withinRange: walksWithinRange +=1
+
+	    withinRangePCT = float( walksWithinRange ) / cyclesPerWalk
+	    if withinRangePCT >= 0.5: longestWalk = DST
+
+	    print( f'Walk length ({DST :0>2d}) -> ({withinRangePCT :.2%})' )
+	
+	return longestWalk
+
+
+
+if __name__ == '__main__':
+	L_Walk = findLongestWalk( withinRange=4, maxWalkLength=31, cyclesPerWalk=1000 )
+
+	print( '\nLongest walk where you will be (on average) less' )
+	print( 'then N steps from the starting position = {}'.format( L_Walk ) )
